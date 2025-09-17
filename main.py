cdc3bd5631f6edf87fba26581b688cb4d9872587
YuvIsNotAnNpc <yuval889306@gmail.com>
2025-09-17 16:31:07 +0300

    ques const

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #The levels of the game are saved in a list
 LEVELS = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 # Question list. In this list, we have all 16 questions
-QUESTION_LIST = ["5 + 5 = ?",
+QUESTION_LIST = ["5 + 5 = ?",aaaaaaaaaaaaaaaaaaaaaaaaaa
                  "10 * 6 = ?",
                  "110 / 5 = ?",
                  "10 + 5 * 3= ?",
@@ -19,11 +19,21 @@ QUESTION_LIST = ["5 + 5 = ?",
 #Answer list question. Every object within the list is a library of the answer to the question.
 # The key is the answer, and the value is a bool if it is the right answer or not.
 #the answer in the index 0 is for the question in the index 0, and so on
-ANSWER_LIST_LIBRARY = [{11:False, 18:False, 10:True, 21:False},{70:False, 18:False, 10:False, 60:True}, {22:True, 15:False, 31:False, 21:False},
-               {45:False, 25:True, 36:False, 21:False},{3.5:False, 4:False, 3.75:True, 4.25:False}, {3.5:False, 4:False, 3.75:True, 4.25:False},
-               {50:False, 56:False, 50.6:True, 60.6:False}, {1300:False, 1000.2:False, 1200:True, 12000:False}, {16:False, 80:False, 64:True, 60:False},
-               {1000:False, 1000.2:False, 1200:True, 12000:False},{10000:True, 15000.2:False, 1000:False, 2000:False}, {480:False, 720:False, 930:True, 1120:False},
-               {100:False, 30:True, 60:False, 170:False}, {-60:False, 40:False, -40:True, -35:False},{100:False, -100:False, -300:True, -150:False},
-               {1000:False, -250:True, 500:False, -1000:False}]
+ANSWER_LIST_LIBRARY = [{11:False, 18:False, 10:True, 21:False},
+                       {70:False, 18:False, 10:False, 60:True},
+                       {22:True, 15:False, 31:False, 21:False},
+                       {45:False, 25:True, 36:False, 21:False},
+                       {3.5:False, 4:False, 3.75:True, 4.25:False},
+                       {3.5:False, 4:False, 3.75:True, 4.25:False},
+                       {50:False, 56:False, 50.6:True, 60.6:False},
+                       {1300:False, 1000.2:False, 1200:True, 12000:False},
+                       {16:False, 80:False, 64:True, 60:False},
+                       {1000:False, 1000.2:False, 1200:True, 12000:False},
+                       {10000:True, 15000.2:False, 1000:False, 2000:False},
+                       {480:False, 720:False, 930:True, 1120:False},
+                       {100:False, 30:True, 60:False, 170:False},
+                       {-60:False, 40:False, -40:True, -35:False},
+                       {100:False, -100:False, -300:True, -150:False},
+                       {1000:False, -250:True, 500:False, -1000:False}]
 
 
